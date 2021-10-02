e9359d134a9fe8619a84aff484975375df56f5bb
MajahaM <majahamkhulisi@gmail.com>
2021-10-02 12:38:42 +0200

    Removed default weather controller

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -2,16 +2,10 @@ using Catalog.API.Data;
 using Catalog.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Catalog.API
 {
@@ -28,7 +22,7 @@ namespace Catalog.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddSingleton<ICatalogContext, CatalogContext>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>

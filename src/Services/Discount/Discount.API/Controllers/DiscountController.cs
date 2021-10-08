b1554a2d8a715f458342f23ed5366151f064c99b
MajahaM <majahamkhulisi@gmail.com>
2021-10-08 17:01:03 +0200

    Added Discount.API Service

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,65 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Coupon>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Coupon>>> GetAllDiscount()
+        {
+            var discounts = await _repository.GetAllDiscount();
+
+            return Ok(discounts);
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var discount = await _repository.GetDiscount(productName);
+
+            return Ok(discount);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody]Coupon discount)
+        {
+            var newDiscount = await _repository.CreateDiscount(discount);
+
+            return CreatedAtAction(nameof(GetDiscount), new { productname = discount.ProductName}, newDiscount);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.NoContent)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon discount)
+        {
+            var newDiscount = await _repository.UpdateDiscount(discount);
+
+            return CreatedAtAction(nameof(GetDiscount), new { productname = discount.ProductName }, newDiscount);
+        }
+
+        [HttpDelete("{productname}")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.NoContent)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productname)
+        {
+           return await _repository.DeleteDiscount(productname);
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,19 @@
+﻿using Discount.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<IEnumerable<Coupon>> GetAllDiscount();
+
+        Task<Coupon> GetDiscount(string productname);
+
+        Task<Coupon> CreateDiscount(Coupon discount);
+
+        Task<Coupon> UpdateDiscount(Coupon discount);
+
+        Task<bool> DeleteDiscount(string productname);
+    }
+}
